705aa82449a698586208ee9690a61aa42e9977ac
Vladymyr2016 <142669270+Vladymyr2016@users.noreply.github.com>
2024-05-07 13:34:41 +0200

    css

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,5 +1,41 @@
+import { useEffect, useState } from 'react';
+import { fetchProducts } from '../../services/API';
+import s from './Shop.module.css';
+
 const Shop = () => {
-  return <div></div>;
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchProducts();
+        setProducts(data);
+        console.log(data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className={s.container}>
+      {products.length > 0 ? (
+        products.map((item) => (
+          <li className={s.item} key={item.id}>
+            <img className={s.img} src={item.thumbnail} alt={item.title} />
+            <h2 className={s.title}>{item.title}</h2>
+            <p className={s.description}>{item.description}</p>
+            <p className={s.price}>{item.price}$</p>
+            <button className={s.btn}>See more</button>
+            <button className={s.btn}>Add to cart</button>
+          </li>
+        ))
+      ) : (
+        <li>No products available</li>
+      )}
+    </div>
+  );
 };
 
 export default Shop;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,19 @@
 import { NavLink } from 'react-router-dom';
+import s from './Navbar.module.css';
 
 const Navbar = () => {
   return (
-    <header>
-      <NavLink to="/">GOOD-SHOP</NavLink>
+    <header className={s.header}>
+      <NavLink className={s.logo} to="/">
+        GOOD-SHOP
+      </NavLink>
       <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/Cart">Cart</NavLink>
+        <NavLink className={s.home} to="/">
+          Home
+        </NavLink>
+        <NavLink className={s.cart} to="/Cart">
+          Cart
+        </NavLink>
       </nav>
     </header>
   );
