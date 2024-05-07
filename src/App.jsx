f43c820c93d2c5bf006c3fa3e07780c044417311
Vladymyr2016 <142669270+Vladymyr2016@users.noreply.github.com>
2024-05-07 10:24:18 +0200

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route></Route>
+        <Route path="/" element={<Layout />}>
+          <Route index />
+          <Route path="cart" />
+          <Route path="products/:producId" />
+        </Route>
       </Routes>
     </>
   );
