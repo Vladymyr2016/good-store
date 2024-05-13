c84a5b4d6a632dde5473035ddce7c3653e287ee3
Vladymyr2016 <142669270+Vladymyr2016@users.noreply.github.com>
2024-05-13 09:47:48 +0200

    css

--- src/Pages/Cart/ProductCart.jsx
+++ src/Pages/Cart/ProductCart.jsx
@@ -1,10 +1,12 @@
+import s from './ProductCart.module.css';
+
 const ProductCart = ({ id, title, price, thumbnail, count }) => {
   return (
-    <li>
-      <div>
-        <img src={thumbnail} aria-live={title} />
-        <p>{title}</p>
-        <div>
+    <li className={s.item}>
+      <div className={s.box}>
+        <img className={s.image} src={thumbnail} aria-live={title} />
+        <p className={s.title}>{title}</p>
+        <div className={s.boxbtn}>
           <button>-</button>
           <p>{count}</p>
           <button>+</button>
